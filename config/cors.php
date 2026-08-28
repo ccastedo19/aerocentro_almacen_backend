98ea0fef0e5a99a9dac18e0f8b6447e68b1246df
ccastedo19 <cesar.castedo1@gmail.com>
2026-08-28 17:10:35 -0400

    re-ajuste de cors para usar con el frontend en vercel

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://aerocentro-almacen-frontend.vercel.app/'],
+    'allowed_origins' => [
+        'https://aerocentro-almacen-frontend.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
