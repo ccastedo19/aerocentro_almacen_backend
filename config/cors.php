44710e09bbdfe334c121ca6703c7e0d7b896b7b2
ccastedo19 <cesar.castedo1@gmail.com>
2026-08-28 16:56:31 -0400

    configuracino de cors con vercel

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://aerocentro-almacen-frontend.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
